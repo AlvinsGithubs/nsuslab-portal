39e874567537903f5041f175b4f69571d6bb24ff
alvin.jang@nsuslab.com <alvin.jang@nsuslab.com>
2025-08-14 20:02:14 +0900

    change menu effect

--- components/Header.tsx
+++ components/Header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC = () => {
                 <a
                   href={link.href}
                   onClick={(e) => handleNavigate(e, link.href)}
-                  className="hover-line-effect text-base font-bold uppercase tracking-wider text-nsus-gray-100 hover:text-nsus-blue transition-colors px-4 py-7 flex items-center"
+                  className="menu-underline text-base font-bold uppercase tracking-wider text-white hover:text-nsus-blue transition-colors px-4 py-7 flex items-center"
                 >
                   {t(link.nameKey)}
                 </a>
